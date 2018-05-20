17fbda995cc650f291e2ef4019ed89bf07a89fbc
Takuya Tokuda <cs.toku.mail@gmail.com>
2018-05-21 03:55:55 +0900

    lockファイルの更新

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,121 +1,93 @@
 import * as webpack from 'webpack';
 import * as path from 'path';
 import * as RemoveAssetsPlugin from 'remove-assets-webpack-plugin';
-import * as StyleLintPlugin from 'stylelint-webpack-plugin';
 import * as ExtractTextPlugin from 'extract-text-webpack-plugin';
 
+const extractStyle = new ExtractTextPlugin('style.amp.css');
+const extractHighlight = new ExtractTextPlugin('hybrid.css');
+
 module.exports = [
-  {
-    entry: {
-      entry: './src/ts/index.ts',
-    },
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'static/assets')
-    },
-    module: {
-      rules: [
-        {
-          enforce: 'pre',
-          test: /\.tsx?$/,
-          exclude: /node_modules/,
-          loader: 'tslint-loader'
+    {
+        mode: 'development',
+        entry: {
+            style: './src/css/style.amp.css',
         },
-        {
-          test: /\.tsx?$/,
-          exclude: /node_modules/,
-          loader: 'ts-loader'
+        output: {
+            filename: 'dummy',
+            path: path.resolve(__dirname, 'dist')
         },
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              'css-loader',
-              {
-                loader: 'postcss-loader',
-                options: {
-                  plugins: (loader) => [
-                    require("postcss-import")({
-                      addDependencyTo: webpack,
-                      path: [
-                        'node_modules/bootstrap/dist/css'
-                      ]
-                    }),
-                    require("postcss-url")(),
-                    require("postcss-cssnext")(),
-                    require("postcss-browser-reporter")(),
-                    require("postcss-reporter")(),
-                    require("postcss-math")(),
-                  ]
-                }
-              }
-            ]
-          }),
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: extractStyle.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {loader: 'css-loader', options: {importLoaders: 1}},
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    plugins: (loader) => [
+                                        require("postcss-import")({
+                                            addDependencyTo: webpack,
+                                            path: []
+                                        }),
+                                        require("postcss-url")(),
+                                        require("postcss-cssnext")(),
+                                        require("postcss-browser-reporter")(),
+                                        require("postcss-reporter")(),
+                                        require("postcss-math")(),
+                                        require('stylelint')(),
+                                    ]
+                                }
+                            }
+                        ]
+                    })
+                },
+                {
+                    test: /\.(png|jpg|gif|svg)$/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000
+                    }
+                },
+            ],
         },
-        {
-          test: /\.(woff2?|ttf|eot|svg)$/,
-          use: [
-              'file-loader'
-          ]
-        },
-      ]
+        plugins: [
+            new RemoveAssetsPlugin(/^dummy$/),
+            extractStyle,
+        ]
     },
-    plugins: [
-      //new StyleLintPlugin(),
-      new ExtractTextPlugin('style.css'),
-    ]
-  },
-  {
-    entry: {
-      style: './src/css/style.amp.css'
-    },
-    output: {
-      filename: 'dummy',
-      path: path.resolve(__dirname, 'layouts/partials')
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              'css-loader',
-              {
-                loader: 'postcss-loader',
-                options: {
-                  plugins: (loader) => [
-                    require("postcss-import")({
-                      addDependencyTo: webpack,
-                      path: [
-                        'node_modules/bootstrap/dist/css'
-                      ]
-                    }),
-                    require("postcss-url")(),
-                    require("postcss-cssnext")(),
-                    require("postcss-browser-reporter")(),
-                    require("postcss-reporter")(),
-                    require("postcss-math")(),
-                  ]
-                }
-              }
-            ]
-          })
+    {
+        mode: 'development',
+        entry: {
+            style: './src/css/hybrid.css',
         },
-        {
-          test: /\.(woff2?|ttf|eot|svg)$/,
-          use: [
-              'file-loader'
-          ]
+        output: {
+            filename: 'dummy',
+            path: path.resolve(__dirname, 'dist')
         },
-      ],
-    },
-    plugins: [
-      new RemoveAssetsPlugin(/^dummy$/),
-      //new StyleLintPlugin(),
-      new ExtractTextPlugin('style.amp.css'),
-    ]
-  }
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: extractHighlight.extract({
+                        fallback: 'style-loader',
+                        use: {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: true,
+                                sourceMap: false
+                            }
+                        }
+                    })
+                },
+            ],
+        },
+        plugins: [
+            new RemoveAssetsPlugin(/^dummy$/),
+            extractHighlight,
+        ]
+    }
 ];
 
