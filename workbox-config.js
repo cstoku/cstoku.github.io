90baea7b29914364a79d2763285a1d827160fc24
Takuya Tokuda <cs.toku.mail@gmail.com>
2018-06-29 18:52:31 +0900

    キャッシュ戦略色々変更

--- workbox-config.js
+++ workbox-config.js
@@ -1,17 +1,65 @@
 module.exports = {
   "globDirectory": "public/",
   "globPatterns": [
-    "**/*.{ttf,html,jpg,png}"
+    "**/*.{ttf,html}",
+    "**/{thumbnail,header}.jpg"
   ],
-  "swDest": "public/sw.js",
+  "swDest": "public/js/sw.js",
+  "modifyUrlPrefix": {
+    '/js': ''
+  },
   "maximumFileSizeToCacheInBytes": 5 * 1024 * 1024,
-  "runtimeCaching": [{
-    urlPattern: new RegExp('^https://cdn.ampproject.org'),
-    handler: 'staleWhileRevalidate',
-    options: {
-      cacheableResponse: {
-        statuses: [0, 200]
+  "runtimeCaching": [
+    {
+      urlPattern: new RegExp('^https://cdn.ampproject.org'),
+      handler: 'staleWhileRevalidate',
+      options: {
+        cacheName: 'amp-components',
+        cacheableResponse: {
+          statuses: [0, 200]
+        }
+      }
+    },
+    {
+      urlPattern: '/',
+      handler: 'networkFirst',
+      options: {
+        cacheName: 'page',
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24
+        }
+      }
+    },
+    {
+      urlPattern: /\.(woff|ttf|eot|otf)/,
+      handler: 'cacheFirst',
+      options: {
+        cacheName: 'assets',
+        expiration: {
+            maxAgeSeconds: 60 * 60 * 24 * 14
+        }
+      }
+    },
+    {
+      urlPattern: /.*\/(thumbnail|header)[^\/]*\.(jpg|png)/,
+      handler: 'cacheFirst',
+      options: {
+        cacheName: 'img-thumbnail',
+        expiration: {
+            maxAgeSeconds: 60 * 60 * 24 * 7
+        }
+      }
+    },
+    {
+      urlPattern: /\.(jpg|png)/,
+      handler: 'cacheFirst',
+      options: {
+        cacheName: 'img',
+        expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 60 * 60 * 24 * 7
+        }
       }
     }
-  }]
+  ]
 };
