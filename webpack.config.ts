7043576c0d13b336365a2a8d8926313dc5260a87
Takuya Tokuda <cs.toku.mail@gmail.com>
2023-02-05 02:23:02 +0900

    Set temporary default mode

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,63 +7,56 @@ import CssMinimizerPlugin from 'css-minimizer-webpack-plugin'
 const env = process.env.NODE_ENV
 const DIST_DIR = path.resolve(__dirname, 'layouts/partials/head/amp/css')
 
-module.exports = [
-  {
-    entry: {
-      style: './src/pcss/style.amp.pcss',
-      hybrid: './src/css/hybrid.css',
-    },
-    output: {
-      filename: '[name].js',
-      path: DIST_DIR,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.p?css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 1,
-                url: false,
-                sourceMap: env !== 'production',
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  plugins: [
-                    [
-                      'postcss-import',
-                      {
-                        addDependencyTo: webpack,
-                        path: []
-                      },
-                    ],
-                    ['postcss-url', {url: 'inline'}],
-                    ['postcss-preset-env', {stage: 0}],
-                    'postcss-browser-reporter',
-                    'postcss-reporter',
-                    'postcss-calc',
-                    //require('stylelint')(),
-                  ],
-                },
-                sourceMap: env !== 'production'
-              }
+module.exports = {
+  mode: 'development',
+  entry: {
+    style: './src/pcss/style.amp.pcss',
+    hybrid: './src/css/hybrid.css',
+  },
+  output: {
+    filename: '[name].js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.p?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              url: false,
+              sourceMap: env !== 'production',
             }
-          ]
-        },
-      ],
-    },
-  optimization: {
-    minimizer: env === 'production' ? [new CssMinimizerPlugin()] : []
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['postcss-import', {addDependencyTo: webpack}],
+                  ['postcss-url', {url: 'inline'}],
+                  ['postcss-preset-env', {stage: 0}],
+                  'postcss-browser-reporter',
+                  'postcss-reporter',
+                  'postcss-calc',
+                  //require('stylelint')(),
+                ],
+              },
+              sourceMap: env !== 'production'
+            }
+          }
+        ]
+      },
+    ],
   },
-    plugins: [
-      new RemoveEmptyScriptsPlugin({}),
-      new MiniCssExtractPlugin(),
-    ]
-  },
-];
+optimization: {
+  minimizer: env === 'production' ? [new CssMinimizerPlugin()] : []
+},
+  plugins: [
+    new RemoveEmptyScriptsPlugin({}),
+    new MiniCssExtractPlugin(),
+  ]
+}
