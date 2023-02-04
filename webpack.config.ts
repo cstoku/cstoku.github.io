5b899f453cc6031e8dab959864dd5789e80bab4a
Takuya Tokuda <cs.toku.mail@gmail.com>
2023-02-05 01:42:07 +0900

    node and packages

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,98 +1,69 @@
-import * as webpack from 'webpack';
-import * as path from 'path';
-import * as RemoveAssetsPlugin from 'remove-assets-webpack-plugin';
-import * as ExtractTextPlugin from 'extract-text-webpack-plugin';
+import * as webpack from 'webpack'
+import * as path from 'path'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import RemoveEmptyScriptsPlugin from 'webpack-remove-empty-scripts'
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin'
 
-const env = process.env.NODE_ENV;
-const extractStyle = new ExtractTextPlugin('style.css');
-const extractHighlight = new ExtractTextPlugin('hybrid.css');
+const env = process.env.NODE_ENV
+const DIST_DIR = path.resolve(__dirname, 'layouts/partials/head/amp/css')
 
 module.exports = [
-    {
-        entry: {
-            style: './src/pcss/style.amp.pcss',
-        },
-        output: {
-            filename: 'dummy',
-            path: path.resolve(__dirname, 'layouts/partials/head/amp/css')
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.pcss$/,
-                    use: extractStyle.extract({
-                        fallback: 'style-loader',
-                        use: [
-                            {   loader: 'css-loader',
-                                options: {
-                                    importLoaders: 1,
-                                    sourceMap: env !== 'production',
-                                    minimize: env === 'production'
-                                }
-                            },
-                            {
-                                loader: 'postcss-loader',
-                                options: {
-                                    plugins: (loader) => [
-                                        require("postcss-import")({
-                                            addDependencyTo: webpack,
-                                            path: []
-                                        }),
-                                        require("postcss-url")(),
-                                        require("postcss-preset-env")({stage: 0}),
-                                        require("postcss-browser-reporter")(),
-                                        require("postcss-reporter")(),
-                                        require("postcss-math")(),
-                                        //require('stylelint')(),
-                                    ],
-                                    sourceMap: env !== 'production' ? 'inline' : false
-                                }
-                            }
-                        ]
-                    })
-                },
-                {
-                    test: /\.(png|jpg|gif|svg)$/,
-                    loader: 'url-loader',
-                    options: {
-                        limit: 10000
-                    }
-                },
-            ],
-        },
-        plugins: [
-            new RemoveAssetsPlugin(/^dummy$/),
-            extractStyle,
-        ]
+  {
+    entry: {
+      style: './src/pcss/style.amp.pcss',
+      hybrid: './src/css/hybrid.css',
     },
-    {
-        entry: {
-            style: './src/css/hybrid.css',
-        },
-        output: {
-            filename: 'dummy',
-            path: path.resolve(__dirname, 'layouts/partials/head/amp/css')
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.css$/,
-                    use: extractHighlight.extract({
-                        fallback: 'style-loader',
-                        use: {
-                            loader: 'css-loader',
-                            options: {
-                                minimize: env === 'production',
-                                sourceMap: env !== 'production'
-                            }
-                        }
-                    })
+    output: {
+      filename: '[name].js',
+      path: DIST_DIR,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.p?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                url: false,
+                sourceMap: env !== 'production',
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    [
+                      'postcss-import',
+                      {
+                        addDependencyTo: webpack,
+                        path: []
+                      },
+                    ],
+                    ['postcss-url', {url: 'inline'}],
+                    ['postcss-preset-env', {stage: 0}],
+                    'postcss-browser-reporter',
+                    'postcss-reporter',
+                    'postcss-calc',
+                    //require('stylelint')(),
+                  ],
                 },
-            ],
+                sourceMap: env !== 'production'
+              }
+            }
+          ]
         },
-        plugins: [
-            new RemoveAssetsPlugin(/^dummy$/),
-            extractHighlight,
-        ]
-    }
+      ],
+    },
+  optimization: {
+    minimizer: env === 'production' ? [new CssMinimizerPlugin()] : []
+  },
+    plugins: [
+      new RemoveEmptyScriptsPlugin({}),
+      new MiniCssExtractPlugin(),
+    ]
+  },
 ];
